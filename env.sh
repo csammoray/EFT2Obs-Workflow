9c5049ff68889de1bad3df29e78f21eb523d4846
Christian Sammoray <christian.sammoray@outlook.com>
2026-05-03 22:12:00 +0200

    updated stuff

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 mamba activate /eos/user/c/csammora/conda_envs/envs/snakemake
 
 export EFT2OBS_DIR=/eft2obs
-export PROC_DIR=/eos/user/c/csammora/EFT2Obs-Workflow/results/process_output
-export CARDS_DIR=/eos/user/c/csammora/EFT2Obs-Workflow/results/cards
+export PROC_DIR=/eos/user/c/csammora/EFT2Obs-Workflow/results/postMidterm/HIG-25-015/CPV/process_output
+export CARDS_DIR=/eos/user/c/csammora/EFT2Obs-Workflow/results/postMidterm/HIG-25-015/CPV/cards
 export TMPDIR=/eos/user/c/csammora/EFT2Obs-Workflow/tmp
